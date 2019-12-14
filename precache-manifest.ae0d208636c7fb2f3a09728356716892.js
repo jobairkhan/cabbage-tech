699942f75b28d57f8423acb2fa55387755d3a972
Jobair <m.j.khan.bd@gmail.com>
2019-12-14 11:21:37 +0000

    Updates

---
+++ precache-manifest.ae0d208636c7fb2f3a09728356716892.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c630f8caef7c99098e5af7c1cd23e800",
+    "url": "/cabbage-tech/index.html"
+  },
+  {
+    "revision": "55b7188e03aeb610ca9d",
+    "url": "/cabbage-tech/static/css/main.c4ab7940.chunk.css"
+  },
+  {
+    "revision": "9b47d0d932765958bd1f",
+    "url": "/cabbage-tech/static/js/2.19c3c994.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cabbage-tech/static/js/2.19c3c994.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55b7188e03aeb610ca9d",
+    "url": "/cabbage-tech/static/js/main.fbf6cb46.chunk.js"
+  },
+  {
+    "revision": "665940167ea1954e406a",
+    "url": "/cabbage-tech/static/js/runtime-main.b723d006.js"
+  },
+  {
+    "revision": "03e91f122aa5fd425abbe23c85546eb0",
+    "url": "/cabbage-tech/static/media/Linearicons-Free.03e91f12.woff2"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "/cabbage-tech/static/media/Linearicons-Free.2f3e9f80.ttf"
+  },
+  {
+    "revision": "65060723fe964f85afa0a82d0bb78cf9",
+    "url": "/cabbage-tech/static/media/Linearicons-Free.65060723.woff"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "/cabbage-tech/static/media/Linearicons-Free.b9b7f23c.eot"
+  },
+  {
+    "revision": "ec161a74448ccf2c18beefe8cca2b44a",
+    "url": "/cabbage-tech/static/media/Linearicons-Free.ec161a74.svg"
+  }
+]);
