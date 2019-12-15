8f90d58d470d3c90a5bb3d099eee6c52c4f94ff7
Jobair <m.j.khan.bd@gmail.com>
2019-12-15 08:49:59 +0000

    add alt

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -7,7 +7,7 @@ class AboutUs extends Component {
                 <div className="container">
                     <div className="row wow fadeInDown p-3 animated" data-wow-duration="1000ms" data-wow-delay="1s">
                         <div className="col-md-3 inline" style={{padding: 5}}>
-                            <img src={process.env.PUBLIC_URL + "/images/1.png"} className="img-responsive" />
+                            <img src={process.env.PUBLIC_URL + "/images/1.png"} className="img-responsive" alt="Md Mazam Ali Khan"/>
                         </div>
                         <div className="col-md-9 inline">
                             <h1>Md. Mazam Ali Khan</h1>
@@ -24,17 +24,17 @@ class AboutUs extends Component {
                     <div className="row">   
                         <div className="col-sm-6 profile wow fadeInRight animated" data-wow-duration="1000ms" data-wow-delay="1s" >
                         <div className="col-md-12">
-                            <img src={process.env.PUBLIC_URL + "/images/2.png"} className="img-responsive" />
+                            <img src={process.env.PUBLIC_URL + "/images/2.png"} className="img-responsive" alt="Jobair"/>
                         </div>
                         <div className="col-md-12">
-                            <h1>Jobair Khan </h1>
+                            <h1>M Jobair Khan </h1>
                             <h2>Software Consultant</h2>
                             <p className="minimize">Jobair Khan accumulated regional and international experience exceeding 10 years of proficiency in team management, software development, project planning, ERP, IT service management and his operation<span>... </span><a href="#" className="more">Show More</a><span style={{ display: 'none' }}>al excellence by managing enterprise level project in PMI framework also drives the quality assurance initiatives for the company. His volume of experience, aptitude and adroit decision-making has culminated into several leading executive positions and board memberships. <a href="#" className="less">Show Less</a></span></p>
                         </div>
                     </div>
                         <div className="col-sm-6 profile wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="1s">
                             <div className="col-md-12">
-                                <img src={process.env.PUBLIC_URL + "/images/3.png"} className="img-responsive"/>
+                                <img src={process.env.PUBLIC_URL + "/images/3.png"} className="img-responsive" alt="Mehedi"/>
                             </div>
                             <div className="col-md-12">
                                 <h1>Mehedi Hassan Khan</h1>
