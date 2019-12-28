2c4d8ed1c8ca01530dc4084278d22088843f7c05
Jobair <m.j.khan.bd@gmail.com>
2019-12-28 01:47:40 +0000

    use ShowMoreText

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
+import ShowMoreText from 'react-show-more-text';
 
 class AboutUs extends Component {
+
+    executeOnClick = (isExpanded) => {
+        console.log(isExpanded);
+    }
+
     render() {
         return (
             <section id="aboutUs" className="section team">
@@ -10,8 +16,8 @@ class AboutUs extends Component {
                 </div>
                 <div className="container">
                     <div className="row wow fadeInDown p-3 animated" data-wow-duration="1000ms" data-wow-delay="1s">
-                        <div className="col-md-3 inline" style={{padding: 5}}>
-                            <img src={process.env.PUBLIC_URL + "/images/1.png"} className="img-responsive" alt="Md Mazam Ali Khan"/>
+                        <div className="col-md-3 inline" style={{ padding: 5 }}>
+                            <img src={process.env.PUBLIC_URL + "/images/1.png"} className="img-responsive" alt="Md Mazam Ali Khan" />
                         </div>
                         <div className="col-md-9 inline">
                             <h1>Md. Mazam Ali Khan</h1>
@@ -28,29 +34,52 @@ class AboutUs extends Component {
                             </h2>
                             <hr className="lines wow zoomIn animated" data-wow-delay="0.3s" />
                         </div>
-                        <div className="row">   
+                        <div className="row">
                             <div className="col-sm-6 profile wow fadeInRight animated" data-wow-duration="1000ms" data-wow-delay="1s" >
-                            <div className="col-md-12">
-                                <img src={process.env.PUBLIC_URL + "/images/2.png"} className="img-responsive" alt="Jobair"/>
+                                <div className="col-md-12">
+                                    <img src={process.env.PUBLIC_URL + "/images/2.png"} className="img-responsive" alt="Jobair" />
+                                </div>
+                                <div className="col-md-12">
+                                    <h1>M Jobair Khan </h1>
+                                    <h2>Software Consultant</h2>
+                                    <ShowMoreText
+                                        /* Default options */
+                                        lines={3}
+                                        more='Show more'
+                                        less='Show less'
+                                        anchorClass=''
+                                        onClick={this.executeOnClick}
+                                        expanded={false}
+                                        width={500}>
+                                        Jobair Khan accumulated regional and international experience exceeding 10 years of proficiency in team management,
+                                        software development, project planning, ERP, IT service management and his operational excellence
+                                        by managing enterprise level project in PMI framework also drives the quality assurance initiatives for the company.
+                                        His volume of experience, aptitude and adroit decision-making has culminated into several leading executive positions and board memberships.
+                                    </ShowMoreText>
+                                </div>
                             </div>
-                            <div className="col-md-12">
-                                <h1>M Jobair Khan </h1>
-                                <h2>Software Consultant</h2>
-                                <p className="minimize">
-                                    Jobair Khan accumulated regional and international experience exceeding 10 years of proficiency in team management, software development, project planning, ERP, IT service management and his operation<span>... </span><a href="#." className="more">Show More</a><span style={{ display: 'none' }}>al excellence by managing enterprise level project in PMI framework also drives the quality assurance initiatives for the company. His volume of experience, aptitude and adroit decision-making has culminated into several leading executive positions and board memberships. <a href="#." className="less">Show Less</a></span>
-                                </p>
-                            </div>
-                        </div>
                             <div className="col-sm-6 profile wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="1s">
                                 <div className="col-md-12">
-                                    <img src={process.env.PUBLIC_URL + "/images/3.png"} className="img-responsive" alt="Mehedi"/>
+                                    <img src={process.env.PUBLIC_URL + "/images/3.png"} className="img-responsive" alt="Mehedi" />
                                 </div>
                                 <div className="col-md-12">
                                     <h1>Mehedi Hassan Khan</h1>
                                     <h2>Project Manager</h2>
-                                    <p className="minimize">
-                                        Mehedi Hassan Khan is Head of Finance, Administration and HR of Cabbage Tech since its foundation. He is responsible for overseeing the overall finance function of Cabbage Tech, driving business growt<span>... </span><a href="." className="more">Show More</a><span style={{ display: 'none' }}>h and profitability and developing corporate strategies. His mission is to continuously build an organization that provides industry best IT Professionals, broad range of technology services and flexible delivery models to the global software development industry so that they get more out of their IT Investments and with greater speed.  <a href="#." className="less">Show Less</a></span>
-                                    </p>
+                                    <ShowMoreText
+                                        /* Default options */
+                                        lines={3}
+                                        more='Show more'
+                                        less='Show less'
+                                        anchorClass=''
+                                        onClick={this.executeOnClick}
+                                        expanded={false}
+                                        width={500}>
+                                        Mehedi Hassan Khan is Head of Finance, Administration and HR of Cabbage Tech since its foundation.
+                                        He is responsible for overseeing the overall finance function of Cabbage Tech, driving business growth and profitability and developing corporate strategies.
+                                        His mission is to continuously build an organization that provides industry best IT Professionals,
+                                        broad range of technology services and flexible delivery models to the global software development industry so that they get more out of their IT Investments and with greater speed.
+
+                                    </ShowMoreText>
                                 </div>
                             </div>
                         </div>
